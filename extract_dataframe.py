250b6dfe1e9a984355ce8fb2da32210283002278
Daniel <danielzelalemheru@gmail.com>
2021-06-24 01:41:16 +0300

    data cleaning, and exploration done

--- extract_dataframe.py
+++ extract_dataframe.py
@@ -162,7 +162,7 @@ if __name__ == "__main__":
                'original_author', 'screen_count', 'followers_count', 'friends_count', 'possibly_sensitive', 'hashtags', 'user_mentions', 'place', 'place_coord_boundaries']
     _, tweet_list = read_json("./data/covid19.json")
     tweet = TweetDfExtractor(tweet_list)
-    # tweet_df = tweet.get_tweet_df()
+    tweet_df = tweet.get_tweet_df(save=True)
     tweet.find_statuses_count()
 
     # use all defined functions to generate a dataframe with the specified columns above
